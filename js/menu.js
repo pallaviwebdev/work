85778a106d5b641f49dba88679756c79035c5471
pallaviwebdev <chatepallavi28@gmail.com>
2023-03-25 17:32:11 +0530

    final project

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+function myfunction(){
+    var x = document.getElementById("hide");
+    if(x.style.display === "block")
+    {
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block"
+    }
+}
